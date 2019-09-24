792cef90650cd3875fc1707b9747e970fdbf96e9
mohammad-shaddad <mohammad@onyx.ws>
2019-09-24 18:07:27 +0300

    Return 404/405 if no definition found

--- src/Tanuki/Simulation/SimulationMiddleware.cs
+++ src/Tanuki/Simulation/SimulationMiddleware.cs
@@ -19,32 +19,64 @@ namespace Onyx.Tanuki.Simulation
 
         public async Task InvokeAsync(HttpContext context, JsonConfiguration config)
         {
+            // Find a matching Path
             string requestPath = context.Request.Path;
-
             Path path = config.Tanuki.Paths.FirstOrDefault(path =>
                path.Uri == requestPath
             );
-
             if (path is null)
             {
-                // TODO: Handle path not found
+                // Path not found
+                // Return 404 - Not Found (https://tools.ietf.org/html/rfc7231#section-6.5.4)
+                await CreateNotFoundResponse(context);
             }
             else
             {
+                // Path found - Match Operation
                 string requestAction = context.Request.Method;
                 Operation operation = path.Operations.FirstOrDefault(operation =>
                     operation.Name.Equals(requestAction, StringComparison.InvariantCultureIgnoreCase)
                 );
-
                 if (operation is null)
                 {
-                    // TODO: Handle operation not found
+                    // Operation not found
+                    // Return 405 - Method Not Allowed (https://tools.ietf.org/html/rfc7231#section-6.5.5)
+                    await CreateMethodNotAllowedResponse(context);
                 }
                 else
                 {
+                    // Operation found
+                    // Simulate operation
                     await context.Response.WriteAsync(operation.Responses.First().Content.First().Examples.First().Value);
                 }
             }
+        }
+
+        /// <summary>
+        /// Creates a 404 Not Found HTTP response
+        /// https://tools.ietf.org/html/rfc7231#section-6.5.4
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        private Task CreateNotFoundResponse(HttpContext context)
+        {
+            context.Response.ContentType = context.Request.ContentType;
+            context.Response.StatusCode = 404;
+            return context.Response.WriteAsync("");
+        }
+
+
+        /// <summary>
+        /// Creates a 405 Method Not Allowed HTTP response
+        /// https://tools.ietf.org/html/rfc7231#section-6.5.5
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        private Task CreateMethodNotAllowedResponse(HttpContext context)
+        {
+            context.Response.ContentType = context.Request.ContentType;
+            context.Response.StatusCode = 405;
+            return context.Response.WriteAsync("");
         }
     }
 }
